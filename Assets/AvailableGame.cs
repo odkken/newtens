f6c791367872bf6fa9a7b2a26bbca68017cc987f
odkken <odkken@gmail.com>
2014-12-14 00:54:29 -0800

    stuff

---
+++ Assets/AvailableGame.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    public class AvailableGame : MonoBehaviour
+    {
+        public HostData HostData;
+        private Text gameNameText;
+        private Text numPlayersText;
+
+        void Start()
+        {
+            gameNameText = transform.FindChild("GameNameText").GetComponent<Text>();
+            numPlayersText = transform.FindChild("NumPlayersText").GetComponent<Text>();
+        }
+
+        public void SetHostData(HostData data)
+        {
+            HostData = data;
+            gameNameText.text = HostData.gameName;
+            numPlayersText.text = "Players: " + HostData.connectedPlayers + "/" + HostData.playerLimit;
+        }
+
+        public void Join()
+        {
+            PanelManager.Instance.GoToPanel(GameObject.Find("TwoPlayerGamePanel").GetComponent<MovablePanel>());
+            Network.Connect(HostData);
+        }
+    }
+}
